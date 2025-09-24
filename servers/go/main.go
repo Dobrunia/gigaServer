206e30a6f76b1f3e36f4590a22eb967d3381fe90
Dobrunia Kostrigin <48620984+Dobrunia@users.noreply.github.com>
2025-09-25 00:48:00 +0300

    Remove .env.example file and hardcode server address and port in Go server implementation

--- servers/go/main.go
+++ servers/go/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -18,15 +17,7 @@ func main() {
     mux := http.NewServeMux()
     mux.HandleFunc("/", handler)
 
-    host := os.Getenv("HOST")
-    if host == "" {
-        host = "127.0.0.1"
-    }
-    port := os.Getenv("GO_SERVER_PORT")
-    if port == "" {
-        port = "3002"
-    }
-    addr := host + ":" + port
+    addr := "127.0.0.1:3002"
     log.Printf("Starting Go server on http://%s", addr)
     if err := http.ListenAndServe(addr, mux); err != nil {
         log.Fatal(err)
