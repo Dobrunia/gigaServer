bb0c937072c82473541769cc81dcd4eff145fdbf
Dobrunia Kostrigin <48620984+Dobrunia@users.noreply.github.com>
2025-11-20 18:56:21 +0300

    Add base enemy and hero configurations with initial stats

--- games/lua/spellrift/src/config/heroes.lua
+++ games/lua/spellrift/src/config/heroes.lua
@@ -1,16 +1,25 @@
+-- Базовый герой
+-- базовое хп 100
+-- прирост хп 5
+-- базовый армор 3
+-- прирост армора 0.3
+-- скорость перед 250
+-- прирост ск 5
+-- радиус подбор опыта 100
+
 local heroes = {
     ["сhronomancer"] = {
         id = "сhronomancer",
         name = "Chronomancer",
-        baseHp = 80,
-        hpGrowth = 10,
-        baseArmor = 0,
-        armorGrowth = 0.5,
-        baseMoveSpeed = 100,
-        speedGrowth = 1,
+        baseHp = 90,
+        hpGrowth = 4,
+        baseArmor = 2,
+        armorGrowth = 0.2,
+        baseMoveSpeed = 230,
+        speedGrowth = 5,
         width = 64,
         height = 64,
-        pickupRange = 100,
+        pickupRange = 200,
         innateSkill = {
             id = "сhronomancer_innate",
             name = "Spell Mastery",
